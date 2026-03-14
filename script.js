cf3333ef6923b1b020635d49612cc8ea10fa9f82
vraj2011 <vrajsanghavi9@gmail.com>
2026-03-15 00:13:23 +0530

    Refactor cart functionality and menu item handling

--- script.js
+++ script.js
@@ -104,20 +104,12 @@ function addMenuItem() {
     let category = document.getElementById("food-category").value;
     let img = document.getElementById("food-img").value;
 
-    let menu = JSON.parse(localStorage.getItem("menu")) || [];
+  let menu = JSON.parse(localStorage.getItem("menu")) || [
+{name:"Cappuccino",price:120,category:"coffee",img:"https://images.unsplash.com/photo-1509042239860-f550ce710b93"},
+{name:"Sandwich",price:160,category:"snacks",img:"https://images.unsplash.com/photo-1551782450-a2132b4ba21d"},
+{name:"Cake",price:180,category:"dessert",img:"https://images.unsplash.com/photo-1551024601-bec78aea704b"}
+];  
 
-    menu.push({
-        name: name,
-        price: price,
-        category: category,
-        img: img
-    });
-
-    localStorage.setItem("menu", JSON.stringify(menu));
-
-    alert("Menu item added!");
-
-}
 function addCart(i) {
 
     cart.push(menu[i]);
